5dcbd20047537a51d232c1c05525f24f9a094b4c
fevunge <developervunge@gmail.com>
2026-04-11 09:51:14 +0100

    refactor: parse CLI arguments and output tokenized args

--- src/presentation/cli/index.ts
+++ src/presentation/cli/index.ts
@@ -1,8 +1,9 @@
 import { ASCII_HEAD } from "@src/shared/const/head";
 import { HELP_MESSAGE } from "@src/shared/const/help";
+import { tokenaizer_args } from "@src/presentation/parser";
 
 export const cli = () => {
 	console.log(ASCII_HEAD);
-	console.log("process.argv: ", process.argv);
-	console.log(HELP_MESSAGE);
+  const args = process.argv.slice(2);
+  console.log(tokenaizer_args(args));
 }
