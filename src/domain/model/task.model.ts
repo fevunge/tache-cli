84130be46feb2a63ee99939bf29658cad2ae25ef
fevunge <developervunge@gmail.com>
2026-04-11 09:50:45 +0100

    feat: refactor TaskModel to include constructor parameters and getter/setter methods

--- src/domain/model/task.model.ts
+++ src/domain/model/task.model.ts
@@ -1,5 +1,39 @@
 export class TaskModel {
-	constructor() {
+	private id: string;
+	private title: string;
+	private description: string[];
+	constructor(id: string, title: string, description: string[]) {
 		
+		this.id = id;
+		this.title = title;
+		this.description = description;
 	}
+
+	
+	public getId() : string {
+		return this.id;
+	}
+
+	public getTitle() : string {
+		return this.title;
+	}
+
+	public getDescription() : string[] {
+		return this.description;
+	}
+
+	public setId(id: string) : void {
+		if (!this.id) {
+			this.id = id;
+		}
+	}
+
+	public setTitle(title: string) : void {
+		this.title = title;
+	}
+
+	public setDescription(description: string[]) : void {
+		this.description = description;
+	}
+	
 }
