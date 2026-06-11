d5ea5ab88bce1ff43189d3960801ec037df375df
fevunge <developervunge@gmail.com>
2026-06-11 09:38:50 +0100

    refactor: replace EventEmitter with async main, create local app folder, integrate commander CLI

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
-import { cli } from "@src/presentation/cli/index";
-import { EventEmitter } from "events";
+import { LocalApplicationService } from "./infrastructure/device/local-application.service";
+import { Command } from "commander";
+import { command_line_interface } from "./presentation/cli";
+import { cli } from "@src/presentation/cli/commandline";
 
-const eventEmitter = new EventEmitter();
 
-eventEmitter.on("start", () => {
-  console.log("Application started");
-})
+async function main() {
 
-cli(eventEmitter);
+  const localApplicationService: LocalApplicationService = new LocalApplicationService();
+  const commander: Command = cli;
+
+  localApplicationService.createLocalApplicationFolder();
+  command_line_interface(commander);
+
+}
+(async () => {
+  await main().catch((error) => {
+    console.error("An error occurred:", error);
+    process.exit(1);
+  });
+});
