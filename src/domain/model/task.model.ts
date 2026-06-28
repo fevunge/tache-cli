89191fb986bf70f68bca25c8375f2c6b279c7813
fevunge <developervunge@gmail.com>
2026-06-28 16:24:36 +0100

    refactor: add timestamps, priority, parent-child, tags, and accessors to TaskModel

--- src/domain/model/task.model.ts
+++ src/domain/model/task.model.ts
@@ -1,21 +1,76 @@
+import { PriorityType } from "@src/domain/types/priority.type";
+import { TaskTagType } from "@src/domain/types/task_tag.type";
+
 export class TaskModel {
 	private id: string;
 	private title: string;
 	private description: string[];
-	constructor(id: string, title: string, description: string[]) {
-		this.id = id;
-		this.title = title;
-		this.description = description;
+	private created_at: Date;
+	private updated_at: Date | null;
+	private priotity: PriorityType;
+	private parent: TaskModel | null;
+	private childreans: TaskModel[];
+	private tags: TaskTagType[];
+	
+	public getTags(): TaskTagType[] {
+		return this.tags;
+	}
+	public addTags(value: TaskTagType) {
+		this.tags.push(value);
+	}
+		
+	public getChildreans(): TaskModel[] {
+		return this.childreans;
+	}
+	public addChildreans(value: TaskModel) {
+		this.childreans.push(value);
+	}
+	
+	
+	public gePriotity(): PriorityType {
+		return this.priotity;
+	}
+	public setPriotity(value: PriorityType) {
+		this.priotity = value;
 	}
 
+	public getParent(): TaskModel | null {
+		return this.parent;
+	}
+	public setParent(value: TaskModel | null) {
+		this.parent = value;
+	}
+	
+	constructor(title: string, ) {
+		this.id = "generate_id";
+		this.title = title;
+		this.priotity = 3;
+		this.description = [];
+		this.tags = [];
+		this.childreans = [];
+		this.created_at = new Date(Date.now());
+		this.updated_at = null;
+		this.parent = null;
+	}
+
+	public getCreated_at(): Date {
+		return this.created_at;
+	}
+	public setCreated_at(value: Date) {
+		this.created_at = value;
+	}
+
+	public getUpdated_at(): Date | null {
+		return this.updated_at;
+	}
+	public setUpdated_at(value: Date) {
+		this.updated_at = value;
+	}
 	public getId() : string {
 		return this.id;
 	}
 	public getTitle() : string {
 		return this.title;
-	}
-	public getDescription() : string[] {
-		return this.description;
 	}
 
 	public setId(id: string) : void {
@@ -28,7 +83,12 @@ export class TaskModel {
 		this.title = title;
 	}
 
-	public setDescription(description: string[]) : void {
-		this.description = description;
-	}	
+	public getDescription(): string[] {
+		return this.description;
+	}
+	public addDescription(value: string) {
+		this.description.push(value);
+	}
+
+
 }
