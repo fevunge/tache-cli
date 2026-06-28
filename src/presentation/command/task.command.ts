30d9aebb1170e505a06927efebe5e19fa275c1f1
fevunge <developervunge@gmail.com>
2026-06-28 16:25:32 +0100

    refactor: update task command to use AddTaskCmdDto and TaskService, add file import support

--- src/presentation/command/task.command.ts
+++ src/presentation/command/task.command.ts
@@ -1,19 +1,21 @@
 import { Command } from "commander";
+import { AddTaskCmdDto } from "../dto/add_task_cmd.dto";
+import { TaskService } from "@src/application/service/task.service";
 
+
+const taskService = new TaskService();
 
 const addTaskCommand = new Command("add")
 	.description("Add a new task")
 	.option("-f, --file <file>", "Specify a file to read task details from")
-	.option("-i, --interactive", "Enter task details interactively")
 	.option("-t, --title <title>", "Set the title of the task")
 	.option("-d, --description <description>", "Set the description of the task")
 	.option("-c, --complete", "Mark the task as complete", false)
-	.action((options: { title: string; description: string; complete: boolean }) => {
-		const { title, description, complete } = options;
-		if (complete) {
-		  console.log(`Creating task "${title}" and marking it as complete...`);
+	.action((options: AddTaskCmdDto) => {
+		if (options.file) {
+		  taskService.addFromFile(options.file)
 		} else {
-		  console.log(`Creating task "${title}" with description "${description}"...`);
+		  console.log(`Trying Create task using "${options.title}" as title with description "${options.description}"...`);
 		}
 	});
 
