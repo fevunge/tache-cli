cf5bb7108dabb08f165dfda40b32eae319f7f9c3
fevunge <developervunge@gmail.com>
2026-07-25 18:06:11 +0100

    feat: refactor addFromFile method to support JSON and CSV file types with validation

--- src/application/service/task.service.ts
+++ src/application/service/task.service.ts
@@ -1,24 +1,39 @@
 import { TaskModel } from "@src/domain/model/task.model";
-import { TaskQueries } from "../queries/task.queries";
-import { TaskUsecases } from "../usecases/task.usecases";
-import { AllowedFileType } from "@src/shared/types/allowed_files.type";
+import { TaskQueries } from "../port/in/queries/task.queries";
+import { TaskUsecases } from "../port/in/usecases/task.usecases";
+import { AllowedFileEnum, AllowedFileType } from "@src/shared/types/allowed_files.type";
 import { validFileType } from "../validation/file.validation";
 import { getFileContent } from "@src/domain/service/file_content.service";
+import { AddTaskCmdDto, AddTaskCmmdSchema } from "@src/presentation/dto/add_task_cmd.dto";
+import { readJson } from "fs-extra";
 
 
 export class TaskService implements TaskUsecases, TaskQueries {
-	addFromFile(fileName: string): TaskModel | null {
-		const fileType: AllowedFileType | null = validFileType(fileName);
-		if (fileType === null) {
-			(async () => {
-				let fileContent: string | null = await getFileContent(fileName);
-				console.log(fileContent);
-			});
-			return (new TaskModel("null"));
-		} else {
-			console.log("Creating from a " + fileType + " file type");
-			return (new TaskModel("example"));
-		}
+  public addFromFile(options: AddTaskCmdDto): void {
+    if (options.file) {
+      const fileType: AllowedFileType | null = validFileType(options.file);
+      const task = new TaskModel();
+      if (fileType == AllowedFileEnum.JSON) {
+        (async () => {
+          const jsonContent = await readJson(options.file!);
+          const jsonParsed = AddTaskCmmdSchema.safeParse(jsonContent);
+          if (jsonParsed.success) {
+            if (jsonParsed.data.title || options.title) {
+              console.log("Error, title is required");
+            }
+            task.setTitle(jsonParsed.data.title || options.title || "");
+            task.setDone(jsonParsed.data.done || options.done || false);
+            task.setNote(jsonParsed.data.note || options.note);
+            task.setPriotity(jsonParsed.data.priority || options.priority || "NORMAL");
+            task.toString();
+          } else {
+            console.log(jsonParsed.error._zod.def);
+          }
+        })();
+      } else if (fileType == AllowedFileEnum.CSV) {
+        console.log("Creating from a " + fileType + " file type");
+      }
+    }
 	}
 
 	public get(id: string): TaskModel {
@@ -27,7 +42,7 @@ export class TaskService implements TaskUsecases, TaskQueries {
 	public all(): TaskModel[] {
 		throw new Error("Method not implemented.");
 	}
-	public add(title: string,): TaskModel {
+	public add(option: AddTaskCmdDto): void {
 		throw new Error("Method not implemented.");
 	}
 	public update(id: string, newTitle: string): TaskModel {
