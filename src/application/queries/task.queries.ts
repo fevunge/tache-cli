46051d00fdb461cf212045eefdc7f044ec89335d
fevunge <developervunge@gmail.com>
2026-07-25 18:04:05 +0100

    refactor: remove obsolete task management interfaces and related services

--- src/application/queries/task.queries.ts
+++
@@ -1,6 +0,0 @@
-import { TaskModel } from "@src/domain/model/task.model";
-
-export interface TaskQueries {
-	get(id: string): TaskModel;
-	all(): TaskModel[];
-}
--- src/application/usecases/task.usecases.ts
+++
@@ -1,8 +0,0 @@
-import { TaskModel } from "@src/domain/model/task.model";
-
-export interface TaskUsecases {
-	add(title: string): TaskModel;
-	addFromFile(file: string): TaskModel | null;
-	update(id: string, newTitle: string): TaskModel;
-	remove(id: string): TaskModel;
-}
