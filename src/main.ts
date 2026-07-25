dc3b82e207e5efa11b78653d228e0d78c426c36a
fevunge <developervunge@gmail.com>
2026-07-25 18:05:37 +0100

    refactor: remove LocalApplicationService instantiation and related method call

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { LocalApplicationService } from "./infrastructure/device/local-application.service";
 import { Command } from "commander";
 import { command_line_interface } from "./presentation/cli";
 import { cli } from "@src/presentation/cli/commandline";
  
-const localApplicationService: LocalApplicationService = new LocalApplicationService();
+
 const commander: Command = cli;
-localApplicationService.createLocalApplicationFolder();
 command_line_interface(commander);
+
+
+
