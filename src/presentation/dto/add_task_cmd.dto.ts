0b2a58fa21629e7ed34bc78d053a794a560fe784
fevunge <developervunge@gmail.com>
2026-07-25 18:05:31 +0100

    feat: redefine AddTaskCmdDto using Zod schema for validation

--- src/presentation/dto/add_task_cmd.dto.ts
+++ src/presentation/dto/add_task_cmd.dto.ts
@@ -1,6 +1,13 @@
-export type AddTaskCmdDto = {
-	title?: string,
-	description?: string,
-	complete: boolean,
-	file?: string
-}
+import { PrioritySchema } from "@src/domain/types/priority.type";
+import { z } from "zod/v4"
+
+export const AddTaskCmmdSchema = z.object({
+  title: z.string().nonempty().optional(),
+  note: z.string().nonempty().optional(),
+  done: z.boolean().default(false),
+  file: z.string().nonempty().optional(),
+  tags: z.string().optional(),
+  priority: PrioritySchema
+});
+
+export type AddTaskCmdDto = z.infer<typeof AddTaskCmmdSchema>;
