577778c263cfa71a19bdef8b1f3be78917388fb3
fevunge <developervunge@gmail.com>
2026-07-25 18:05:42 +0100

    refactor: streamline command line interface by removing redundant command definitions

--- src/presentation/cli/index.ts
+++ src/presentation/cli/index.ts
@@ -1,28 +1,15 @@
 import { Command } from "commander";
 import { taskCommand } from "../command/task.command";
+import { aboutCommand } from "../command/about.command";
+import { projectCommand } from "../command/project.command";
+import { meCommand } from "../command/me.command";
+import { scheduleCommand } from "../command/schedule.command";
 
 export const command_line_interface = (commandLine: Command) => {
 	commandLine.addCommand(taskCommand());
-	commandLine
-	  .command("project")
-	  .description("List all tasks")
-	  .action(() => {
-		console.log("Listing all tasks...");
-	  });
-
-	commandLine
-	  .command("me")
-	  .description("Read a task by ID")
-	  .action((id: string) => {
-		console.log(`Reading task ${id}...`);
-	  });
-
-	commandLine
-	  .command("schedule")
-	  .description("Mark a task as complete")
-	  .action((id: string) => {
-		console.log(`Marking task ${id} as complete...`);
-	  });
-
+	commandLine.addCommand(aboutCommand());
+	commandLine.addCommand(projectCommand());
+	commandLine.addCommand(meCommand());
+	commandLine.addCommand(scheduleCommand());
 	commandLine.parse();
 }
