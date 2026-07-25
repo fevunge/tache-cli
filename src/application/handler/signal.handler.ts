309e185ecdb4c3e197b107adfcc39806491bac96
fevunge <developervunge@gmail.com>
2026-07-25 18:03:25 +0100

    refactor: remove unused signal handler implementation

--- src/application/handler/signal.handler.ts
+++
@@ -1,8 +0,0 @@
-export const SetSignalHandler = (signal: NodeJS.Signals, handler: () => void) => {
-	process.on(signal, () => {
-		console.log(`Received ${signal}, exiting...`);
-		if (handler)
-			handler();
-		process.exit(0);
-	});
-}
