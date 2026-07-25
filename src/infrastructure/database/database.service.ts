6c7c1a3904e945e530b4066c77d0adde01580441
fevunge <developervunge@gmail.com>
2026-07-25 18:04:52 +0100

    feat: update DatabaseService to use drizzle ORM for database connection and initialization

--- src/infrastructure/database/database.service.ts
+++ src/infrastructure/database/database.service.ts
@@ -1,24 +1,65 @@
-import { db } from "./database.config";
+import { drizzle } from "drizzle-orm/bun-sqlite";
 
 export class DatabaseService {
-	private database_instance = db;
-	constructor() {}
-
+	private database_instance;
+	constructor() {
+		try {
+			this.database_instance = (drizzle("database.db"));
+			console.log("Database connection initialized", this.isOk())
+		} catch (error) {
+			this.database_instance = null,
+			console.log("Error init database connection", this.isOk());
+			console.log(error, typeof(error));	
+		}	 
+	}
 	public getDatabaseInstance() {
 		return this.database_instance;
 	}
 
-	public async closeDatabaseConnection() {
+	public isOk() {
+		return this.database_instance !== null
+	}
+	public closeDatabaseConnection() {
 		this.database_instance.$client.close();
+		console.log("Database connection closed");
 	}
 
-	public async isOpenDatabaseConnection() {
+	public isOpenDatabaseConnection() {
 		return this.database_instance.$client.open;
 	}
 
 	public async initializeDatabase() {
-		// create tables if not exist
 		await this.database_instance.run(`CREATE TABLE IF NOT EXISTS users (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name TEXT NOT NULL,
+			email TEXT NOT NULL UNIQUE,
+			password TEXT NOT NULL,
+			
+			updated_at DATETIME,
+			created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+		)`);
+		await this.database_instance.run(`CREATE TABLE IF NOT EXISTS tasks (
+			id TEXT PRIMARY KEY AUTOINCREMENT,
+			title TEXT NOT NULL,
+			description TEXT,
+			email TEXT NOT NULL UNIQUE,
+			password TEXT NOT NULL,
+			updated_at DATETIME,
+			priotity TEXT NOT NULL,
+			created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+		)`);
+		await this.database_instance.run(`CREATE TABLE IF NOT EXISTS projects (
+			id TEXT PRIMARY KEY AUTOINCREMENT,
+			name TEXT NOT NULL,
+			description TEXT NOT NULL UNIQUE,
+			local_path TEXT,
+			remote_path TEXT,
+			has_version_control BOOL DEFAULT FALSE,
+			version_control_type TEXT DEFAULT "none",
+			updated_at DATETIME,
+			created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+		)`);
+		await this.database_instance.run(`CREATE TABLE IF NOT EXISTS schedules (
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
 			name TEXT NOT NULL,
 			email TEXT NOT NULL UNIQUE,
