fd82e7e8c4727f95866c297d3f142f00d9fc32cd
fevunge <developervunge@gmail.com>
2026-07-26 13:56:30 +0100

    fix: import and invoke hello function from vlogging in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { Command } from "commander";
+import { hello } from "@vlogging";
 import { command_line_interface } from "./presentation/cli";
 import { cli } from "@src/presentation/cli/commandline";
  
 
 const commander: Command = cli;
+hello();
 command_line_interface(commander);
 
 
